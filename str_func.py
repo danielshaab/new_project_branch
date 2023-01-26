764e772c896058721bd32700f904facf338544a7
Danil Dergachev <ddergachev2001@gmail.com>
2023-01-26 20:07:05 +0300

    made some descriptions with docstrings for all functions

--- str_func.py
+++ str_func.py
@@ -1,4 +1,10 @@
 def upper_word(word):
-"""this function converts each word to upper register"""
+    """this function converts each word to upper register"""
     word = word.upper()
+    return word
+
+
+def upper_each_word(word):
+    """this function converts each word's first letter in a sentence to upper register """
+    word = str(word.title())
     return word
